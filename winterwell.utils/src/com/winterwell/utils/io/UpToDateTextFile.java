8153301ba58402e83532876c70a28d5549d26751
Daniel Winterstein <daniel@sodash.com>
2017-03-03 15:26:13 +0000

    uptodate

---
+++ winterwell.utils/src/com/winterwell/utils/io/UpToDateTextFile.java
@@ -0,0 +1,69 @@
+package com.winterwell.utils.io;
+
+import java.io.File;
+import java.util.concurrent.Callable;
+
+import com.winterwell.utils.Utils;
+import com.winterwell.utils.io.FileUtils;
+
+/**
+ * Checks if the file has a modified timestamp & reloads if it has.
+ * @author daniel
+ */
+// TODO would a java 8 file watcher make this more/less efficient?
+public final class UpToDateTextFile {
+	boolean autoReload = true;
+	private final File file;
+	private String txt;
+	private long lastMod;
+	private Callable onUpdate;
+	
+	/**
+	 * @param file Does not have to exist.
+	 */
+	public UpToDateTextFile(File file) {
+		assert ! file.exists() || file.isFile() : file.getAbsolutePath();
+		this.file = file;
+	}
+	
+	public File getFile() {
+		return file;
+	}
+
+	/**
+	 * Called just after a reload. 
+	 * @param callable Can be null
+	 */
+	public void onUpdate(Callable callable) {
+		this.onUpdate = callable;
+	}
+
+	public void setAutoReload(boolean autoReload) {
+		this.autoReload = autoReload;		
+	}
+	
+	/**
+	 * The file's text. "" if the file does not exist. 
+	 */
+	public String toString() {
+		if (txt==null || (autoReload && lastMod < file.lastModified())) {			
+			load();			
+		}
+		return txt==null? "" : txt;
+	}
+
+	/**
+	 * @throws RuntimeException if anything goes wrong
+	 */
+	private void load() {
+		if ( ! file.exists()) return;
+		try {
+			txt = FileUtils.read(file);
+			lastMod = file.lastModified();
+			assert ! Utils.isBlank(txt) : file;
+			if (onUpdate!=null) onUpdate.call();
+		} catch (Exception e) {
+			throw Utils.runtime(e);
+		}
+	}
+}
