ce18fdc1dd41eaf869259078ff2a8b4e842e0045
Daniel Winterstein <daniel@sodash.com>
2017-03-06 11:20:20 +0000

    DepContext

---
+++ winterwell.utils/src/com/winterwell/utils/DepContext.java
@@ -0,0 +1,63 @@
+package com.winterwell.utils;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+public class DepContext implements Closeable {
+
+	boolean closed;
+	DepContext parent;
+	private Object ckey;
+
+	public DepContext(DepContext parent, Object contextKey) {
+		this.ckey = contextKey;
+		this.parent = parent;
+	}
+
+	@Override
+	public String toString() {
+		return "DepContext [closed=" + closed + ", parent=" + parent + ", ckey=" + ckey + "]";
+	}
+
+	@Override
+	public void close() throws IOException {
+		if (parent==null) throw new IllegalStateException("Cannot close top-level context");
+		this.closed = true;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((ckey == null) ? 0 : ckey.hashCode());
+		result = prime * result + (closed ? 1231 : 1237);
+		result = prime * result + ((parent == null) ? 0 : parent.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DepContext other = (DepContext) obj;
+		if (ckey == null) {
+			if (other.ckey != null)
+				return false;
+		} else if (!ckey.equals(other.ckey))
+			return false;
+		if (closed != other.closed)
+			return false;
+		if (parent == null) {
+			if (other.parent != null)
+				return false;
+		} else if (!parent.equals(other.parent))
+			return false;
+		return true;
+	}
+	
+	
+}
--- winterwell.utils/src/com/winterwell/utils/Dependency.java
+++ winterwell.utils/src/com/winterwell/utils/Dependency.java
@@ -1,5 +1,6 @@
 package com.winterwell.utils;
 
+import java.util.NoSuchElementException;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Supplier;
 
@@ -12,21 +13,30 @@ import java.util.function.Supplier;
  */
 public final class Dependency {
 
-	static ConcurrentHashMap<Class, Object> stash = new ConcurrentHashMap<>();
+	static ConcurrentHashMap<DKey, Object> stash = new ConcurrentHashMap<>();
 	
-	static ConcurrentHashMap<Class, Supplier> factory = new ConcurrentHashMap<>();
+	static ConcurrentHashMap<DKey, Supplier> factory = new ConcurrentHashMap<>();
 	
 	public static <X> void setSupplier(Class<X> klass, boolean singleton, Supplier<X> supplier) {
+		DepContext ctxt = getContext();
 		if (singleton) {
 			Supplier supplier2 = () -> {
 				X x = supplier.get();
-				stash.put(klass, x);
+				stash.put(key(klass, ctxt), x);
 				return x;
 			};
-			factory.put(klass, supplier2);
+			factory.put(key(klass, ctxt), supplier2);
 		}
-		factory.put(klass, supplier);
+		factory.put(key(klass, ctxt), supplier);
 	}
+
+	
+	
+	private static DKey key(Class klass, DepContext ctxt) {
+		return new DKey(klass, ctxt);
+	}
+
+
 
 	/**
 	 * 
@@ -35,7 +45,8 @@ public final class Dependency {
 	 * @return value
 	 */
 	public static <X> X set(Class<X> klass, X value) {
-		stash.put(klass, value);
+		DepContext ctxt = getContext();
+		stash.put(key(klass, ctxt), value);
 		return value;
 	}
 	
@@ -46,29 +57,107 @@ public final class Dependency {
 	 * @return
 	 */
 	public static <X> X get(Class<X> class1) {
-		X x = (X) stash.get(class1);
-		if (x!=null) return x;
-		Supplier<X> s = factory.get(class1);
-		if (s!=null) {
-			x = s.get();
-			// don't store factory output. make it fresh. ??
-//			stash.put(class1, x);
-			return x;
+		DepContext ctxt = getContext();
+		return get(class1, ctxt);
+	}
+
+	public static <X> X get(Class<X> class1, DepContext ctxt) {
+		assert ! ctxt.closed;
+		while(ctxt!=null) {			
+			X x = (X) stash.get(key(class1, ctxt));
+			if (x!=null) return x;
+			Supplier<X> s = factory.get(key(class1, ctxt));
+			if (s!=null) {
+				x = s.get();
+				// don't store factory output. make it fresh. ??
+	//			stash.put(class1, x);
+				return x;
+			}
 		}
-		// try a default constructor
-		try {
-			x = class1.newInstance();
-			stash.put(class1, x);
-			return x;
-		} catch (InstantiationException | IllegalAccessException e) {
-			throw Utils.runtime(e);
+		return null;
+	}
+	
+	private static ThreadLocal<DepContext> context = new ThreadLocal<DepContext>() {
+		protected DepContext initialValue() {
+			return new DepContext(null, null);
+		};
+	};
+	
+	public static DepContext setContext(Object contextKey) {
+		DepContext ctxt = getContext();
+		DepContext newContext = new DepContext(ctxt, contextKey);
+		context.set(newContext);
+		return newContext;
+	}
+	
+	public static DepContext getContext() {
+		DepContext ctxt = context.get();
+		while(true) {			
+			if ( ! ctxt.closed) return ctxt;
+			ctxt = ctxt.parent;
+			context.set(ctxt);
+			// since the null-parent top contexts can't be closed, this should always finish
 		}
 	}
 	
-	public static boolean isSet(Class klass) {
-		if (stash.containsKey(klass)) return true;
-		if (factory.containsKey(klass)) return true;
+	public static boolean isSet(Class class1) {
+		DepContext ctxt = getContext();
+		while(ctxt != null) {
+			DKey klass = key(class1, ctxt);
+			if (stash.containsKey(klass)) return true;
+			if (factory.containsKey(klass)) return true;
+			ctxt = ctxt.parent;
+		}
 		return false;
 	}
 
+}
+
+
+final class DKey {
+
+	private final DepContext context;
+	private final Class klass;
+
+	public DKey(Class klass, DepContext context) {
+		this.klass = klass;
+		this.context = context;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((context == null) ? 0 : context.hashCode());
+		result = prime * result + ((klass == null) ? 0 : klass.hashCode());
+		return result;
+	}
+
+	@Override
+	public String toString() {
+		return "DKey [context=" + context + ", klass=" + klass + "]";
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DKey other = (DKey) obj;
+		if (context == null) {
+			if (other.context != null)
+				return false;
+		} else if (!context.equals(other.context))
+			return false;
+		if (klass == null) {
+			if (other.klass != null)
+				return false;
+		} else if (!klass.equals(other.klass))
+			return false;
+		return true;
+	}
+	
 }
