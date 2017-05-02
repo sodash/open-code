3e6efbda73e708527ecaff68829ab8a1eff6ed07
Daniel Winterstein <daniel@sodash.com>
2017-05-02 05:50:28 +0100

    sync

--- src/com/winterwell/gson/FlexiGson.java
+++ src/com/winterwell/gson/FlexiGson.java
@@ -16,55 +16,17 @@
 
 package com.winterwell.gson;
 
-import java.io.EOFException;
-import java.io.IOException;
-import java.io.Reader;
-import java.io.StringReader;
-import java.io.StringWriter;
-import java.io.Writer;
-import java.lang.reflect.Array;
-import java.lang.reflect.Field;
-import java.lang.reflect.Type;
-import java.math.BigDecimal;
-import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.EnumMap;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
-
-import com.winterwell.gson.internal.$Gson$Preconditions;
-import com.winterwell.gson.internal.ConstructorConstructor;
-import com.winterwell.gson.internal.Excluder;
-import com.winterwell.gson.internal.Primitives;
-import com.winterwell.gson.internal.Streams;
-import com.winterwell.gson.internal.bind.ArrayTypeAdapter;
-import com.winterwell.gson.internal.bind.CollectionTypeAdapterFactory;
-import com.winterwell.gson.internal.bind.DateTypeAdapter;
-import com.winterwell.gson.internal.bind.EnumMapTypeAdapter;
-import com.winterwell.gson.internal.bind.JsonAdapterAnnotationTypeAdapterFactory;
-import com.winterwell.gson.internal.bind.JsonTreeReader;
-import com.winterwell.gson.internal.bind.JsonTreeWriter;
-import com.winterwell.gson.internal.bind.LateBinding;
-import com.winterwell.gson.internal.bind.MapTypeAdapterFactory;
-import com.winterwell.gson.internal.bind.ObjectTypeAdapter;
-import com.winterwell.gson.internal.bind.ReflectiveTypeAdapterFactory;
-import com.winterwell.gson.internal.bind.RuntimeTypeAdapterFactory;
-import com.winterwell.gson.internal.bind.SqlDateTypeAdapter;
-import com.winterwell.gson.internal.bind.TimeTypeAdapter;
-import com.winterwell.gson.internal.bind.TypeAdapters;
-import com.winterwell.gson.reflect.TypeToken;
-import com.winterwell.gson.stream.JsonReader;
-import com.winterwell.gson.stream.JsonToken;
-import com.winterwell.gson.stream.JsonWriter;
-import com.winterwell.gson.stream.MalformedJsonException;
 
 /**
  * Gson, with a classname that won't conflict with other jars 
  * @author Daniel Winterstein
  */
 public final class FlexiGson extends Gson {
+
+	public static Map toJSONObject(Object src) {
+		String json = toJSON(src);
+		return fromJSON(json);
+	}
 }
 
