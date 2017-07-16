6040cc9b830a0555cb7114b12964ba3d585b9fd0
Daniel Winterstein <daniel@sodash.com>
2017-07-16 12:56:13 +0100

    sync

--- winterwell.datalog/src-js/components/DashboardPage.jsx
+++ winterwell.datalog/src-js/components/DashboardPage.jsx
@@ -56,6 +56,7 @@ class DashboardPage extends React.Component {
 		let cdata = pivot(mydata, "'byEvent' -> 'buckets' -> bi -> {key, 'byTime' -> 'buckets' -> bi2 -> bucket}", 
 						"key -> bucket");
 		
+		// TODO pull out sub-data on myCount -> sum
 		let tdata = pivot(mydata, "'byTag' -> 'buckets' -> bi -> {key, 'byTime' -> 'buckets' -> bi2 -> bucket}", 
 						"key -> bucket");
 
@@ -120,6 +121,8 @@ const FiltersWidget = () => {
 		filters = DataStore.getValue(livePath) || {};
 		DataStore.setValue(path, filters, false);
 	}
+		// <Misc.PropControl path={path} item={filters} prop='events' label='events' />
+		// <Misc.PropControl path={path} item={filters} prop='publisher' label='Publisher' />
 	return (<div className='well'>
 		<Misc.PropControl path={path} item={filters} prop='dataspace' label='Dataspace e.g. "gl" or "default"' />
 		<Misc.PropControl path={path} item={filters} prop='tags' label='tags' />
