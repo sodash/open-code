4c01f8a241ea378950d91ff6423e6615594eccb0
Roscoe <roscoe@winterwell.com>
2017-07-21 12:38:18 +0100

    Add build script for youagain-java-client

---
+++ youagain-java-client/src/com/winterwell/youagain/client/BuildYouAgainJavaClient.java
@@ -0,0 +1,36 @@
+
+package com.winterwell.youagain.client;
+
+import java.io.File;
+import java.util.Set;
+import java.util.logging.Level;
+
+import com.winterwell.bob.tasks.CopyTask;
+import com.winterwell.bob.tasks.EclipseClasspath;
+import com.winterwell.bob.tasks.JarTask;
+import com.winterwell.bob.tasks.RSyncTask;
+import com.winterwell.bob.tasks.SCPTask;
+
+import com.winterwell.utils.io.FileUtils;
+
+import jobs.BuildBob;
+import jobs.BuildDepot;
+import jobs.BuildMaths;
+import jobs.BuildDataLog;
+import jobs.BuildUtils;
+import jobs.BuildWeb;
+import jobs.BuildWinterwellProject;
+
+public class BuildYouAgainJavaClient extends BuildWinterwellProject {
+
+	public BuildYouAgainJavaClient() {
+		super(new File(FileUtils.getWinterwellDir(), "open-code/youagain-java-client"));
+		setIncSrc(true);
+	}
+
+	@Override
+	public void doTask() throws Exception {
+		super.doTask();
+	}
+
+}
