278af9f313bed9b2ddc2016a06b69a8609683bb3
Daniel Winterstein <daniel@sodash.com>
2017-07-25 22:30:42 +0100

    sync

--- winterwell.webappbase/build/com/winterwell/web/app/BuildWWAppBase.java
+++ winterwell.webappbase/build/com/winterwell/web/app/BuildWWAppBase.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 import com.winterwell.bob.BuildTask;
 import com.winterwell.es.BuildESJavaClient;
 import com.winterwell.utils.io.FileUtils;
+import com.winterwell.youagain.client.BuildYouAgainJavaClient;
 
 import jobs.BuildBob;
 import jobs.BuildDataLog;
@@ -27,7 +28,8 @@ public class BuildWWAppBase extends BuildWinterwellProject {
 				new BuildBob(),
 				new BuildWeb(),
 				new BuildESJavaClient(),
-				new BuildFlexiGson()
+				new BuildFlexiGson(),
+				new BuildYouAgainJavaClient()
 				);
 	}
 	
