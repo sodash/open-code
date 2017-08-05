7e8e0906ed133d959d0ae04edbeb2771292567d1
Daniel Winterstein <daniel@sodash.com>
2017-08-05 18:23:16 +0100

    thinking in code - combining objectdistribution 2nd-order style

---
+++ winterwell.maths/src/com/winterwell/maths/stats/distributions/discrete/AndCombinator.java
@@ -0,0 +1,36 @@
+package com.winterwell.maths.stats.distributions.discrete;
+
+import com.winterwell.maths.classifiers.WilsonScoreInterval;
+
+/**
+ * experimental
+ * combine 2 distributions, using 2nd order probability reasoning
+ * @author daniel
+ *
+ */
+public class AndCombinator<X> {
+
+	void combine(ObjectDistribution<X> a, ObjectDistribution<X> b) {
+		
+		a and b are both samples, giving pa, pb which are estimates of true-p
+		the estimates have a variance, related to sample size 
+		
+		// Wikipedia: The central limit theorem applies poorly to this distribution with a sample size less than 30 or where the proportion is close to 0 or 1.
+		
+		double aw = a.getTotalWeight();
+		double bw = b.getTotalWeight();
+		
+		for(X k : a) {
+			double na = a.prob(k);
+			double nb = b.prob(k);
+			
+			double pa = na / aw;
+			double pb = nb / bw;
+			
+			
+//			WilsonScoreInterval.getInterval(error, p, n)
+		}
+		
+	}
+	
+}
