56074ee638edc12fc220537d3c32349bc06b5539
Daniel Winterstein <daniel@sodash.com>
2017-08-05 19:10:36 +0100

    hm

--- winterwell.maths/src/com/winterwell/maths/stats/distributions/discrete/AndCombinator.java
+++ winterwell.maths/src/com/winterwell/maths/stats/distributions/discrete/AndCombinator.java
@@ -5,6 +5,16 @@ import com.winterwell.maths.classifiers.WilsonScoreInterval;
 /**
  * experimental
  * combine 2 distributions, using 2nd order probability reasoning
+ * 
+ * Suppose we have observations for 
+ * 
+ * x,y -- few observations
+ * x -- lots of observations
+ * y -- lots of observations
+ * 
+ * How should we combine them?
+ * 
+ * 
  * @author daniel
  *
  */
@@ -12,8 +22,8 @@ public class AndCombinator<X> {
 
 	void combine(ObjectDistribution<X> a, ObjectDistribution<X> b) {
 		
-		a and b are both samples, giving pa, pb which are estimates of true-p
-		the estimates have a variance, related to sample size 
+//		a and b are both samples, giving pa, pb which are estimates of true-p
+//		the estimates have a varianPce, related to sample size 
 		
 		// Wikipedia: The central limit theorem applies poorly to this distribution with a sample size less than 30 or where the proportion is close to 0 or 1.
 		
