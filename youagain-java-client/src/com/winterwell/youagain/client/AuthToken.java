92cd68d4674a666559b76d1bca7d1b44a75910e8
Daniel Winterstein <daniel@sodash.com>
2017-09-08 09:43:35 +0100

    sync

--- youagain-java-client/src/com/winterwell/youagain/client/AuthToken.java
+++ youagain-java-client/src/com/winterwell/youagain/client/AuthToken.java
@@ -3,7 +3,7 @@ package com.winterwell.youagain.client;
 import com.winterwell.web.data.XId;
 
 /**
- * TODO merge with DBAuth
+ * TODO merge with DBAuth and make both JWT token based
  * 
  * A token identifying and authorising.
  * @author daniel
