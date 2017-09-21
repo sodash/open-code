e71bad91b177744272d00b2e26d794a97de5d501
DA <daniel.appel.winterwell@gmail.com>
2017-09-21 13:52:31 +0100

    made the datalogger publish process able to run parallel syncing and can update the cluster more efficiently. Also fixed broken functions that only ran for one iteration instead of on a loop (you know, for a cluster-wide pushout).

--- winterwell.datalog/publish-lg.sh
+++ winterwell.datalog/publish-lg.sh
@@ -2,32 +2,45 @@
 
 # Publish-lg.sh  a script to publish files to the LG production or test server(s)
 
+####
+#CHECKING TO SEE IF YOUR MACHINE HAS THE REQUIRED SOFTWARE
+####
+if [ $(which parallel-ssh) = "" ]; then
+	echo -e "In order to use this publishing script, you will need Parallel-SSH installed on your machine"
+	echo -e "install Parallel-SSH with 'sudo apt-get install pssh'"
+	exit 1
+fi
+####
+#END OF CHECKS
+####
+
+
+
+####
+#SETTING THE ARRAYS OF POTENTIAL SERVERS
+####
 PRODUCTION=(gl-es-03.soda.sh gl-es-04.soda.sh gl-es-05.soda.sh)
 TEST='hugh.soda.sh'
-LOCALPUBLISH='localmachine'
-SSHPARAMS='-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
+####
+#END OF TARGET ARRAYS
+####
 
-##Is this a production pushout or a test pushout?
+####
+#Is this a production pushout or a test pushout?
+####
 TYPEOFPUSHOUT=$1
 CLEANPUBLISH=$2
 
 
-
-
+####
+#ACCEPTING AND PROCESSING THE ARGUMENTS
+####
 case $1 in
-	production)
+	production|PRODUCTION)
 	echo "this is a PRODUCTION pushout"
-	TARGET=${PRODUCTION[*]}
+	TARGET=$PRODUCTION
 	;;
-	PRODUCTION)
-	echo "this is a PRODUCTION pushout"
-	TARGET=${PRODUCTION[*]}
-	;;
-	test)
-	echo "this is a TEST pushout"
-	TARGET=$TEST
-	;;
-	TEST)
+	test|TEST)
 	echo "this is a TEST pushout"
 	TARGET=$TEST
 	;;
@@ -38,27 +51,15 @@ case $1 in
 esac
 
 case $2 in
-	frontend)
+	frontend|FRONTEND)
 		echo "This publishing process will only sync the FRONTEND files, and will not sync backend files"
 		PUBLISHLEVEL='frontend'
 	;;
-	FRONTEND)
-		echo "This publishing process will only sync the FRONTEND files, and will not sync backend files"
-		PUBLISHLEVEL='frontend'
-	;;
-	backend)
+	backend|BACKEND)
 		echo "This publishing process will only sync the BACKEND files, and will not sync the frontend files"
 		PUBLISHLEVEL='backend'
 	;;
-	BACKEND)
-		echo "This publishing process will only sync the BACKEND files, and will not sync the frontend files"
-		PUBLISHLEVEL='backend'
-	;;
-	everything)
-		echo "This publishing process will publish BOTH the FRONTEND and BACKEND files"
-		PUBLISHLEVEL='everything'
-	;;
-	EVERYTHING)
+	everything|EVERYTHING)
 		echo "This publishing process will publish BOTH the FRONTEND and BACKEND files"
 		PUBLISHLEVEL='everything'
 	;;
@@ -71,11 +72,7 @@ case $2 in
 esac
 
 case $3 in
-	clean)
-		echo "this publishing process is going to clear out the target server's directories before syncing"
-		CLEANPUBLISH='true'
-	;;
-	CLEAN)
+	clean|CLEAN)
 		echo "this publishing process is going to clear out the target server's directories before syncing"
 		CLEANPUBLISH='true'
 	;;
@@ -84,77 +81,114 @@ case $3 in
 		CLEANPUBLISH='false'
 	;;
 esac
+####
+#END OF ARGUMENTS
+####
 
+####
+#PRECAUTIONARY REMOVAL OF A TMP FILE
+####
+rm /tmp/target.list.txt
+####
+#END OF PRECAUTIONARY REMOVAL OF A TMP FILE
+####
 
-for server in ${TARGET[*]}; do
-	if [[ $CLEANPUBLISH = 'true' ]]; then
-		echo -e "***WARNING***"
-		echo -e "This Script will now clean/clear out all files/directories before syncing"
-		echo -e "You now have 5 seconds to interupt this process if you do not want this to happen"
-		sleep 1
-		CURRENTCOUNT='5'
-		while  [[ $CURRENTCOUNT != '1' ]]; do
-			echo -e "$CURRENTCOUNT"
-			sleep 1
-			CURRENTCOUNT=$((CURRENTCOUNT-1))
-		done
-		ssh $SSHPARAMS winterwell@$server 'rm -rf /home/winterwell/lg.good-loop.com/lib/*'
-		ssh $SSHPARAMS winterwell@$server 'rm -rf /home/winterwell/lg.good-loop.com/web/*'
-		ssh $SSHPARAMS winterwell@$server 'rm -rf /home/winterwell/lg.good-loop.com/config/*'
-		echo -e "Cleaning of $server 's lg.good-loop.com directory is now complete."
+####
+#CREATING TEXT FILE ARRAY OF TARGETTED SERVERS
+####
+printf '%s\n' "${TARGET[@]}" >> /tmp/target.list.txt
+####
+#END OF CREATING TEXT FILE ARRAY OF TARGETTED SERVERS
+####
+
+####
+#SETTING THE FUNCTIONS FOR THE LEVEL OF PUBLISH
+####
+function frontend_publish {
+	echo -e "> Converting LESS files to CSS..."
+	build-less.sh
+	echo ""
+	echo -e "> Syncing the 'web' directory..."
+	parallel-rsync -h /tmp/target.list.txt --user=winterwell --recursive web /home/winterwell/lg.good-loop.com/
+	echo ""
+	echo -e "> Syncing the 'package.json' file..."
+	parallel-rsync -h /tmp/target.list.txt --user=winterwell package.json /home/winterwell/lg.good-loop.com/
+	echo ""
+	echo -e "> Syncing the 'webpack.config.js' file..."
+	parallel-rsync -h /tmp/target.list.txt --user=winterwell webpack.config.js /home/winterwell/lg.good-loop.com/
+	echo ""
+	echo -e "> Syncing the 'src-js' directory..."
+	parallel-rsync -h /tmp/target.list.txt --user=winterwell --recursive src-js /home/winterwell/lg.good-loop.com/
+	echo ""
+	echo -e "> Satisfying NPM dependencies..."
+	parallel-ssh -h /tmp/target.list.txt --user=winterwell 'cd /home/winterwell/lg.good-loop.com && npm i'
+	echo ""
+	echo -e "> Webpacking..."
+	parallel-ssh -h /tmp/target.list.txt --user=winterwell 'cd /home/winterwell/lg.good-loop.com && webpack -p'
+	echo ""
+	echo -e "frontend(s) have been updated for..."
+	cat /tmp/target.list.txt
+	echo ""
+}
+
+function backend_publish {
+	echo -e "> Syncing fresh JARs..."
+	parallel-rsync -h /tmp/target.list.txt --user=winterwell --recursive tmp-lib /home/winterwell/lg.good-loop.com/lib
+	echo ""
+	echo -e "> Restarting the lg process on target(s)"
+	parallel-ssh -h /tmp/target.list.txt --user=winterwell 'sudo service lg restart'
+	echo ""
+	echo -e "backend(s) have been updated for..."
+	cat /tmp/target.list.txt
+	echo ""
+}
+####
+#END OF SETTING THE FUNCTIONS FOR THE LEVEL OF PUBLISH
+####
+
+####
+#SETTING THE 'CLEANPUBLISH' FUNCTION
+####
+function clean_publish {
+	if [[ $PUBLISHLEVEL != 'everything' ]]; then
+		echo -e "Silly goose! you cannot perform a 'clean-out' unless you are publishing both the frontend AND the backend"
+		echo -e "If you really want to 'clean' for this publish, please use the 'everything' argument for the publishlevel"
+		exit 3
 	fi
+	echo -e "***WARNING***"
+	echo -e "This Script will now clean/clear out all files/directories before syncing"
+	echo -e "You now have 5 seconds to interupt this process if you do not want this to happen"
+	sleep 1
+	CURRENTCOUNT='5'
+	while  [[ $CURRENTCOUNT != '1' ]]; do
+		echo -e "$CURRENTCOUNT"
+		sleep 1
+		CURRENTCOUNT=$((CURRENTCOUNT-1))
+	done
+	parallel-ssh -h /tmp/target.list.txt --user=winterwell 'rm -rf /home/winterwell/lg.good-loop.com/lib/*'
+	parallel-ssh -h /tmp/target.list.txt --user=winterwell 'rm -rf /home/winterwell/lg.good-loop.com/web/*'
+	parallel-ssh -h /tmp/target.list.txt --user=winterwell 'rm -rf /home/winterwell/lg.good-loop.com/config/*'
+	echo -e "Cleaning of the lg.good-loop.com directory is now complete for all targets."
+}
+####
+#END OF SETTING THE 'CLEANPUBLISH'S' FUNCTION
+####
 
-
-	function frontend_publish {
-		echo -e "Converting LESS files to CSS..."
-		build-less.sh
-		rsync -rhP web winterwell@$server:/home/winterwell/lg.good-loop.com/
-		rsync -rhP package.json winterwell@$server:/home/winterwell/lg.good-loop.com/
-		rsync -rhP webpack.config.js winterwell@$server:/home/winterwell/lg.good-loop.com/
-		rsync -rhP src-js winterwell@$server:/home/winterwell/lg.good-loop.com/
-		echo -e "Satisfying NPM dependencies..."
-		ssh winterwell@$server 'cd /home/winterwell/lg.good-loop.com && npm i'
-		echo -e "Webpacking..."
-		ssh winterwell@$server 'cd /home/winterwell/lg.good-loop.com && webpack -p'
-		echo -e "$server 's frontend has been updated and published"
-	}
-
-	function backend_publish {
-		rsync -rhP tmp-lib/* winterwell@$server:/home/winterwell/lg.good-loop.com/lib/
-		echo -e "Restarting the lg process on $server..."
-		if [[ ${TARGET[*]} = 'hugh.soda.sh' ]]; then
-			ssh winterwell@$server 'service lg restart'
-		else
-			ssh winterwell@$server 'sudo service lg restart'
+####
+#PERFORMING THE PUBLISH
+####
+case "$PUBLISHLEVEL" in
+	frontend)
+		frontend_publish
+	;;
+	backend)
+		backend_publish
+	;;
+	everything)
+		if [[ $CLEANPUBLISH = 'true' ]]; then
+			clean_publish
 		fi
-	}
-
-	case $PUBLISHLEVEL in
-		frontend)
-			frontend_publish
-			exit 2
-		;;
-		FRONTEND)
-			frontend_publish
-			exit 2
-		;;
-		backend)
-			backend_publish
-			exit 2
-		;;
-		BACKEND)
-			backend_publish
-			exit 2
-		;;
-		everything)
-			frontend_publish
-			backend_publish
-			exit 2
-		;;
-		EVERYTHING)
-			frontend_publish
-			backend_publish
-			exit 2
-		;;
-	esac
-done
+		backend_publish
+		frontend_publish
+	;;
+esac
