3f3fbe1e25f17a95d15e3678986aeb6241d17ca3
Daniel Winterstein <daniel@sodash.com>
2017-10-03 21:23:56 +0100

    refactor version.properties code into a Bob task

---
+++ winterwell.bob/src/com/winterwell/bob/tasks/MakeVersionPropertiesTask.java
@@ -0,0 +1,71 @@
+package com.winterwell.bob.tasks;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.util.Map;
+import java.util.Properties;
+
+import com.winterwell.bob.BuildTask;
+import com.winterwell.utils.StrUtils;
+import com.winterwell.utils.containers.Containers;
+import com.winterwell.utils.io.FileUtils;
+import com.winterwell.utils.log.Log;
+import com.winterwell.utils.time.Time;
+import com.winterwell.utils.web.XStreamUtils;
+
+public class MakeVersionPropertiesTask extends BuildTask {
+
+	private File configDir;
+	private File appDir;
+
+
+	public MakeVersionPropertiesTask() {
+		appDir = FileUtils.getWorkingDirectory();
+		configDir = new File("config").getAbsoluteFile();
+	}
+	
+	public MakeVersionPropertiesTask setAppDir(File appDir) {
+		this.appDir = appDir;
+		return this;
+	}
+	
+	public MakeVersionPropertiesTask setConfigDir(File localConfigDir) {
+		this.configDir = localConfigDir;
+		return this;
+	}
+	
+	@Override
+	protected void doTask() throws Exception {
+		if ( ! configDir.exists()) {
+			Log.i(LOGTAG, "make directory "+configDir);
+			configDir.mkdirs();
+		}
+		// create the version properties
+		File creolePropertiesForSite = new File(configDir, "version.properties");
+		Properties props = creolePropertiesForSite.exists()? FileUtils.loadProperties(creolePropertiesForSite)
+							: new Properties();
+		// set the publish time
+		props.setProperty("publishDate", new Time().toISOString());
+		try {
+			// set info on the git branch
+			String branch = GitTask.getGitBranch(appDir);
+			props.setProperty("branch", branch);
+			// ...and commit IDs
+			Map<String, Object> info = GitTask.getLastCommitInfo(appDir);
+			for(String k : info.keySet()) {
+				Object v = info.get(k);
+				if (v==null) continue;
+				props.setProperty("lastCommit."+k, v.toString());
+			}
+		} catch(Exception ex) {
+			// oh well;
+			Log.d("git.info.error", ex);
+		}
+
+		// save
+		BufferedWriter w = FileUtils.getWriter(creolePropertiesForSite);
+		props.store(w, null);
+		FileUtils.close(w);
+	}
+
+}
