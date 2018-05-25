00259febd3629ddf3aeb7a5948a166e6b5c934e1
Daniel Winterstein <daniel@winterwell.com>
2018-05-25 10:47:03 +0100

    sync

---
+++ winterwell.datalog/src/com/winterwell/datalog/DataLogHttpClient.java
@@ -0,0 +1,12 @@
+package com.winterwell.datalog;
+
+import com.winterwell.datalog.server.DataServlet;
+
+/**
+ * Get data via the DataLog web service (i.e. call DataServlet)
+ * @author daniel
+ *
+ */
+public class DataLogHttpClient {
+
+}
