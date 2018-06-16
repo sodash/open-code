3b13dac3d86b6835edeaa3f0349a49847d569cad
Daniel Winterstein <daniel@sodash.com>
2018-06-16 18:05:20 +0100

    maven-dep

--- winterwell.bob/build/jobs/BuildDataLog.java
+++ winterwell.bob/build/jobs/BuildDataLog.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.Collection;
 
 import com.winterwell.bob.BuildTask;
+import com.winterwell.bob.tasks.MavenDependencyTask;
 import com.winterwell.utils.io.FileUtils;
 
 public class BuildDataLog extends BuildWinterwellProject {
@@ -13,6 +14,16 @@ public class BuildDataLog extends BuildWinterwellProject {
 		super("winterwell.datalog");
 	}	
 
+	@Override
+	public void doTask() throws Exception {
+		
+		MavenDependencyTask mdt = new MavenDependencyTask();
+		mdt.addDependency("com.hazelcast","hazelcast", "3.10.2");
+		mdt.run();
+		
+		super.doTask();
+	}
+	
 	@Override
 	public Collection<? extends BuildTask> getDependencies() {
 		return Arrays.asList(new BuildUtils(), new BuildWeb());
