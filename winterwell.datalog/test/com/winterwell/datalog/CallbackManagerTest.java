ca43532233c5e6c0a6e518d1a53a6eed5ea68645
Daniel Winterstein <daniel@sodash.com>
2018-06-29 18:13:18 +0100

    wip

---
+++ winterwell.datalog/test/com/winterwell/datalog/CallbackManagerTest.java
@@ -0,0 +1,21 @@
+package com.winterwell.datalog;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.winterwell.utils.time.Time;
+
+public class CallbackManagerTest {
+
+	@Test
+	public void test() {
+		DataLogEvent dle = new DataLogEvent("minview", 1);
+		dle.time = new Time();
+		
+		CallbackManager cm = new CallbackManager();
+		Callback callback = new Callback("gl", "minview", "http://localas.good-loop.com");
+		cm.consume2_doCallback(dle, callback);
+	}
+
+}
