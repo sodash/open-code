bf457f32336ce3043a9a8d94ad77ea0974a599f1
Daniel Winterstein <daniel@winterwell.com>
2018-07-27 16:25:20 +0100

    sync

--- winterwell.web/src/com/winterwell/web/app/HttpServletWrapper.java
+++ winterwell.web/src/com/winterwell/web/app/HttpServletWrapper.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.winterwell.utils.Printer;
 import com.winterwell.utils.ReflectionUtils;
 import com.winterwell.utils.Utils;
 import com.winterwell.utils.log.Log;
@@ -85,7 +86,7 @@ public class HttpServletWrapper extends HttpServlet {
 		} else {
 			Log.w(wex.getClass().getSimpleName(), ex);
 		}
-		WebUtils2.sendError(wex.code, wex.getMessage()+" \n\n"+ReflectionUtils.getSomeStack(8), resp);
+		WebUtils2.sendError(wex.code, wex.getMessage()+" \n\n"+Printer.toString(wex, true), resp);
 	}
 
 	@Override
