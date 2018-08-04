b1d319c6ab81f6dcbc7eab0af952c3a8fef2dd57
Daniel Winterstein <daniel@sodash.com>
2018-08-04 19:57:14 +0100

    IHasXId

---
+++ winterwell.web/src/com/winterwell/web/data/IHasXId.java
@@ -0,0 +1,16 @@
+package com.winterwell.web.data;
+
+/**
+ * Interface for objects with an id.
+ * 
+ * Objects implementing this SHOULD have a field <code>String xid</code>
+ * which uses the toString() value of an XId
+ * 
+ * @author daniel
+ *
+ */
+public interface IHasXId {
+
+	XId getXId();
+
+}
