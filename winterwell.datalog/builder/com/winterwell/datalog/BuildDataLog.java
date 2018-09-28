ddefad8456319357a16acd22eb677d4753d8e934
Daniel Winterstein <daniel@winterwell.com>
2018-09-28 15:59:00 +0100

    moved bob files

---
+++ winterwell.datalog/builder/com/winterwell/datalog/BuildDataLog.java
@@ -0,0 +1,37 @@
+
+package com.winterwell.datalog;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Collection;
+
+import com.winterwell.bob.BuildTask;
+
+import jobs.BuildUtils;
+import jobs.BuildWeb;
+import jobs.BuildWinterwellProject;
+
+public class BuildDataLog extends BuildWinterwellProject {
+
+	public BuildDataLog() {
+		super("winterwell.datalog");
+	}	
+
+	@Override
+	public Collection<? extends BuildTask> getDependencies() {
+		return Arrays.asList(new BuildUtils(), new BuildWeb());
+	}
+	
+	@Override
+	public void doTask() throws Exception {	
+		super.doTask();
+		doTest();
+			
+	}
+
+	@Override
+	protected File getTestBinDir() {
+		return new File(projectDir, "bin.test");
+	}
+
+}
--- winterwell.datalog/builder/com/winterwell/datalog/PublishDataServer.java
+++ winterwell.datalog/builder/com/winterwell/datalog/PublishDataServer.java
@@ -1,3 +1,4 @@
+package com.winterwell.datalog;
 
 
 
