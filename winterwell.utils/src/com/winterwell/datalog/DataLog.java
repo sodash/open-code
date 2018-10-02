98d9f5451c83b1f12ac1d87e06b382bd8a4ed1f8
Daniel Winterstein <daniel@winterwell.com>
2018-10-02 17:12:37 +0100

    sync

--- winterwell.utils/src/com/winterwell/datalog/DataLog.java
+++ winterwell.utils/src/com/winterwell/datalog/DataLog.java
@@ -411,7 +411,7 @@ public class DataLog {
 	 * @return tagBits
 	 */
 	static String[] check(String... tagBits) {		
-		assert tagBits != null;
+		assert tagBits != null : "DataLog - null tagBits";
 		assert tagBits.length != 0;
 		assert ! (tagBits.length==1 && tagBits[0] == null);
 		for(int i=0; i<tagBits.length; i++) {
