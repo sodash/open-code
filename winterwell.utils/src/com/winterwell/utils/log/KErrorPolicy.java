2515d107b91548e6058a33d3e3a098c796e63d22
Daniel Winterstein <daniel@winterwell.com>
2018-10-18 15:19:05 +0100

    sync

--- winterwell.utils/src/com/winterwell/utils/log/KErrorPolicy.java
+++ winterwell.utils/src/com/winterwell/utils/log/KErrorPolicy.java
@@ -1,5 +1,8 @@
 package com.winterwell.utils.log;
 
+import com.winterwell.utils.TodoException;
+import com.winterwell.utils.Utils;
+
 public enum KErrorPolicy {
 	/** Keep the error as is (throw an exception if this is not possible) */
 	ACCEPT,
@@ -23,5 +26,27 @@ public enum KErrorPolicy {
 	/** Raise hell. */
 	THROW_EXCEPTION,
 	/** Ask the user?! */
-	ASK
+	ASK;
+
+	
+	public static <X> X process(KErrorPolicy ep, Throwable e) {
+		// default to throw
+		if (ep==null) ep = KErrorPolicy.THROW_EXCEPTION;		
+		if (e==null) e = new RuntimeException();
+		switch(ep) {
+		case DIE:
+			Log.e("dieonerror", e);
+			System.exit(1);
+		case THROW_EXCEPTION:
+			throw Utils.runtime(e);
+		case REPORT:
+			Log.e("error", e);
+		case ACCEPT: case IGNORE:
+			return null;
+		case ASK: case DELETE_CAUSE:
+			Log.e("error", new TodoException("Handle "+ep+" for "+e));
+			throw Utils.runtime(e);
+		}
+		return null;
+	}
 }
