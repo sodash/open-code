cde927d954c414372ef63c669fbaf0927f46da02
Daniel Winterstein <daniel@sodash.com>
2018-10-30 22:03:01 +0000

    must.should.must bugfix

--- winterwell.webappbase/test/com/winterwell/web/app/AppUtilsTest.java
+++ winterwell.webappbase/test/com/winterwell/web/app/AppUtilsTest.java
@@ -3,6 +3,7 @@ package com.winterwell.web.app;
 import static org.junit.Assert.*;
 
 import java.util.List;
+import java.util.Map;
 
 import org.junit.Test;
 
@@ -19,7 +20,12 @@ public class AppUtilsTest {
 			List ptre = sq.getParseTree();
 			assert ! ptre.get(0).equals(SearchQuery.KEYWORD_OR);
 			BoolQueryBuilder esf = AppUtils.makeESFilterFromSearchQuery(sq, null, null);
+			Map esmap = (Map) esf.getUnderlyingMap().get("bool");
+			System.out.println(esmap);
 			System.out.println(esf);
+			// top level must be must
+			assert esmap.containsKey("must") : esmap;
+			assert ! esmap.containsKey("should") : esmap;
 		}
 		if (false) { // meh - we'd like it to handle this as a top-level and -- but lets fix by using unambiguous queries
 			String q = "user:wwvyfncgobrxvwqablhe@trk OR user:mark@winterwell.com@email (evt:spend OR evt:spendadjust OR evt:donation)";
