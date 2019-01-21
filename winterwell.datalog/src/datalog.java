81e030f1259dcdc18d898b9a364ba6da142951c3
DW-Stross <daniel@good-loop.com>
2019-01-21 15:32:47 +0000

    starter Java classes

---
+++ winterwell.datalog/src/datalog.java
@@ -0,0 +1,13 @@
+import com.winterwell.datalog.server.DataLogServer;
+
+/**
+ * convenience for command-line starting
+ * @author daniel
+ *
+ */
+public class datalog {
+
+	public static void main(String[] args) {
+		DataLogServer.main(args);
+	}
+}
