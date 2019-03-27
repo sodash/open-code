49b939ddedea386623ae948388c0d6258fe4b3a5
DW-Stross <daniel@good-loop.com>
2019-03-27 16:48:18 +0000

    sync

--- winterwell.web/src/com/winterwell/web/data/package-info.java
+++ winterwell.web/src/com/winterwell/web/data/package-info.java
@@ -1,9 +1,7 @@
 
 /**
- * What are these creole/SoDash classes doing here in a utils library?
- * <p>
- * Well, {@link com.winterwell.web.data.XId} has turned out to be a really nifty id wrapper (hence utils). 
- * But if we changed the package, we'd break lots of xstream-serialised objects.  
+ * Why is this here in generic utils?
+ * {@link com.winterwell.web.data.XId} has turned out to be a really nifty id wrapper. 
  * 
  * @author daniel
  */
