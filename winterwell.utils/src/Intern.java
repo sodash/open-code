aa6327aa77644b64e26e346e8ba54ffa8da5a78b
DW-Stross <daniel@good-loop.com>
2019-05-16 11:56:10 +0100

    Intern

--- winterwell.utils/src/Intern.java
+++ winterwell.utils/src/Intern.java
@@ -1,8 +1,9 @@
 
+import java.lang.ref.WeakReference;
 import java.util.WeakHashMap;
 import java.util.function.BiFunction;
 
-import com.google.common.cache.CacheBuilder;
+import com.winterwell.utils.Mutable;
 
 /**
  * A memory-safe alternative to Java's String.intern.
@@ -19,15 +20,29 @@ public class Intern {
 	 * Hm: Or should we use a cache, e.g. CacheBuilder or HalfLifeMap?
 	 * Note: do not use CacheBuilder's weakKeys -- this relies on == testing.
 	 */
-	static final WeakHashMap<String, Boolean> strings = new WeakHashMap<>();
+	static final WeakHashMap<String, WeakReference<String>> strings = new WeakHashMap<>();
 		
 	
 	public static String get(String s) {
-		Mutable.Ref result = new Mutable.Ref();
-		strings.compute(s, (k,v) -> {
-			// ??
+		Mutable.Ref<String> result = new Mutable.Ref();
+		strings.compute(s, (k,wr) -> {
+			// cached?
+			if (wr!=null) {
+				String v = wr.get();
+				if (v!=null) {
+					result.value = v; 
+					return wr;
+				}
+			}
+			// set fresh
+			result.value = s;
+			return new WeakReference(s);
 		});
 		return result.value;
 	}
 	
+	public static boolean contains(String s) {
+		WeakReference<String> wr = strings.get(s);
+		return wr!=null && wr.get()!=null;
+	}
 }
---
+++ winterwell.utils/test/InternTest.java
@@ -0,0 +1,56 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.winterwell.utils.Utils;
+
+public class InternTest {
+
+	@Test
+	public void testGet() {
+		String foo1 = "foo";
+		String foo2 = new StringBuilder("f").append("oo").toString();
+		assert foo1 != foo2;
+		String if1 = Intern.get(foo1);
+		String if2 = Intern.get(foo2);
+		String if3 = Intern.get(foo2);
+		String if4 = Intern.get(foo1);
+		assert if1 == if2;
+		assert if1 == if3;
+		assert if3 == if4;
+	}
+
+	
+//	@Test 
+	// Uncomment this to run the test
+	// BUT: this can legitimately fail (it depends on the behaviour of the GC, which isn't fixed)
+	// So do NOT include this test in automated acceptance testing.
+	public void testOverload() {
+		// try to force a GC to hit Intern with loads of data
+		// I can't think of a good way to make this a reliable test. ^Dan W
+		StringBuilder rem = null;
+		for(int j=0; j<10000; j++) {
+			StringBuilder s1 = new StringBuilder();
+			for(int i=0; i<10000; i++) {
+				s1.append(Utils.getRandomString(3));
+			}			
+			String is = Intern.get(s1.toString());
+			if (rem==null) {
+				rem = s1;
+			}
+		}		
+		Utils.sleep(5000);
+		System.gc();
+		Utils.sleep(5000);
+		// NB: hopefully rem's string has been dropped from Intern		
+		String s1 = rem.toString();
+		String s2 = new String(rem.toString());
+		
+		assert ! Intern.contains(s1) : "oh well - it didnt get flushed";
+		
+		String if1 = Intern.get(s1);
+		String if2 = Intern.get(s2);
+		assert if1 == if2;
+	}
+	
+}
