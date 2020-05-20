c352ffb903ff02cc833633397dbcad9f92709f9a
Daniel Winterstein <daniel@good-loop.com>
2020-05-20 11:54:59 +0100

    BD

---
+++ winterwell.depot/builder/com/winterwell/bob/wwjobs/BuildDepot.java
@@ -0,0 +1,20 @@
+package com.winterwell.bob.wwjobs;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.winterwell.bob.BuildTask;
+
+public class BuildDepot extends BuildWinterwellProject {
+
+	public BuildDepot() {
+		super("winterwell.depot");
+	}	
+
+	@Override
+	public List<BuildTask> getDependencies() {
+		return Arrays.asList(new BuildUtils(), new BuildWeb());
+	}
+
+
+}
