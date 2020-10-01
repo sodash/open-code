7b3a040cb5a2fb54a485a1a44e5c4911cb029d87
kai <kaijuneer@gmail.com>
2020-10-01 12:30:01 +0100

    new branch for shrinking data

---
+++ winterwell.datalog/test/com/winterwell/datalog/server/AggregationTest.java
@@ -0,0 +1,14 @@
+package com.winterwell.datalog.server;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AggregationTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
