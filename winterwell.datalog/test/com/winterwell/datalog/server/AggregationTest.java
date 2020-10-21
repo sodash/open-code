11db99bfbc8284c24617b0f3d548add48b70afe9
kai <kaijuneer@gmail.com>
2020-10-21 13:33:47 +0100

    java driver to bulk data into local ES index

--- winterwell.datalog/test/com/winterwell/datalog/server/AggregationTest.java
+++ winterwell.datalog/test/com/winterwell/datalog/server/AggregationTest.java
@@ -2,6 +2,7 @@ package com.winterwell.datalog.server;
 
 import java.util.List;
 import java.util.Map;
+import java.util.ArrayList;
 import java.util.HashMap;
 import static org.junit.Assert.*;
 import org.junit.Test; 
@@ -10,8 +11,17 @@ import org.eclipse.jetty.util.ajax.JSON;
 import com.winterwell.gson.JsonParser;
 import com.winterwell.gson.JsonElement;
 import com.winterwell.gson.JsonObject;
+import com.winterwell.gson.FlexiGson;
 import com.winterwell.gson.JsonArray;
 import com.winterwell.datalog.Dataspace;
+import com.winterwell.es.UtilsForESTests;
+import com.winterwell.es.client.BulkRequestBuilder;
+import com.winterwell.es.client.BulkResponse;
+import com.winterwell.es.client.ESConfig;
+import com.winterwell.es.client.ESHttpClient;
+import com.winterwell.es.client.IndexRequestBuilder;
+import com.winterwell.es.client.KRefresh;
+import com.winterwell.utils.Dep;
 import com.winterwell.utils.Printer;
 import com.winterwell.utils.containers.ArrayMap;
 import com.winterwell.utils.web.WebUtils;
@@ -25,6 +35,7 @@ public class AggregationTest {
 	private static DataLogServer server;
 	private static String ENDPOINT;
 	private static final CharSequence DATASPACE = new Dataspace("gl"); //query from the local ES index
+	public final static String INDEX = "datalog.compressed"; //bulk into newly created local ES index
 	
 	public void initDataTest() {
 		if (ENDPOINT!=null) return;
@@ -51,7 +62,7 @@ public class AggregationTest {
 	}
 	
 	@Test
-	public void testCreateNewIndexData() {
+	public void testBulkNewIndexData() {
 		initDataTest();
 		FakeBrowser fb = fb();
 		// create breakdown that does aggregation based on time interval of domains
@@ -64,42 +75,47 @@ public class AggregationTest {
 		String data = resp.getData().string();
 		Printer.out(data);
 		
+		// prepare to bulk aggregated data
+		Dep.setIfAbsent(FlexiGson.class, new FlexiGson());
+		Dep.setIfAbsent(ESConfig.class, new ESConfig());
+		ESConfig esconfig = Dep.get(ESConfig.class);
+		if ( ! Dep.has(ESHttpClient.class)) Dep.setSupplier(ESHttpClient.class, false, ESHttpClient::new);
+		ESHttpClient esc = Dep.get(ESHttpClient.class);
+		BulkRequestBuilder bulk = esc.prepareBulk();
+		
+		// parse aggregated data 
 		JsonElement jelement = new JsonParser().parse(data);
 	    JsonObject  jobject = jelement.getAsJsonObject();
 	    jobject = jobject.getAsJsonObject("by_domain_time");
 	    JsonArray jarray = jobject.getAsJsonArray("buckets");
 	    JsonArray child_jarray;
+	    // each j is a different domain
+	    int i = 0;
 	    for (JsonElement j : jarray) {
 	    	jobject = j.getAsJsonObject();
 	    	String domain_name = jobject.get("key").getAsString();
 	    	jobject = jobject.getAsJsonObject("by_time");
 	    	child_jarray = jobject.getAsJsonArray("buckets");
 	    	for (JsonElement child : child_jarray) {
-	    		JsonObject compressed_json = new JsonObject();
-	    		compressed_json.addProperty("domain", domain_name);
-	    		compressed_json.addProperty("time", child.getAsJsonObject().get("key_as_string").getAsString());
-	    		compressed_json.addProperty("count", child.getAsJsonObject().get("doc_count").getAsDouble());
-	    		System.out.println("The domain is: " + domain_name);
-	    		System.out.println("The time is: " + child.getAsJsonObject().get("key_as_string").getAsString());
-	    		System.out.println("The count is: " + child.getAsJsonObject().get("doc_count").getAsDouble());
-	    		System.out.println(compressed_json.toString());
-	    		break;
+	    		String time = child.getAsJsonObject().get("key_as_string").getAsString();
+	    		double doc_count = child.getAsJsonObject().get("doc_count").getAsDouble();
+	    		IndexRequestBuilder pi = esc.prepareIndex(INDEX, "compressed_"+i);
+	    		i = i + 1; //increment document id
+				pi.setBodyMap(new ArrayMap("domain", domain_name, "time", time, "count", doc_count));
+				bulk.add(pi);
 	    	}
-	    	break;
 	    }
+	    bulk.setRefresh(KRefresh.WAIT_FOR);
+		bulk.setDebug(true);
+		BulkResponse br = bulk.get();
+		assert ! br.hasErrors() : br.getError();
 		
-	    /*
-		resp = parseJSON(data, "by_domain_time");
-		data = resp.getData().string();
-		Printer.out(data);
-		resp = parseJSON(data, "buckets");
-		data = resp.getData().string();
-		// data is a list with multiple buckets, each bucket corresponds to a domain
-		Printer.out(data);
-		*/
-	
+		// sanity check if data is written to local ES
+		Map<String, Object> got = esc.get(INDEX, "simple", "compressed_30");
+		System.out.println(got);
 	}
-	
+	/* 
+	//troublesome to use
 	private JSend parseJSON(String data, String keyname) {
 		Map jobj = (Map) JSON.parse(data);
 		System.out.println(jobj.keySet());
@@ -109,7 +125,7 @@ public class AggregationTest {
 		thing.setJsonObject(_data);
 		JSend resp = jsend.setData(thing);
 		return resp;
-	}
+	} */
 	
 	private FakeBrowser fb() {
 		FakeBrowser fb = new FakeBrowser();
