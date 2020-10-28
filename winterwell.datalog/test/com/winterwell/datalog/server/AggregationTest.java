c8c7af602b925d3f1c314b8297224cb491d23d29
kai <kaijuneer@gmail.com>
2020-10-28 12:53:30 +0000

    include more fields

--- winterwell.datalog/test/com/winterwell/datalog/server/AggregationTest.java
+++ winterwell.datalog/test/com/winterwell/datalog/server/AggregationTest.java
@@ -24,6 +24,7 @@ import com.winterwell.es.client.KRefresh;
 import com.winterwell.utils.Dep;
 import com.winterwell.utils.Printer;
 import com.winterwell.utils.containers.ArrayMap;
+import com.winterwell.utils.containers.Pair2;
 import com.winterwell.utils.web.WebUtils;
 import com.winterwell.utils.web.WebUtils2;
 import com.winterwell.web.FakeBrowser;
@@ -53,7 +54,7 @@ public class AggregationTest {
 		String json = fb.getPage(ENDPOINT+"/data", new ArrayMap(
 				"name","test-1",
 				"dataspace", DATASPACE,
-				"breakdown", "domain/time"
+				"breakdown", "domain/time, evt/campaign/domain/pub/vert/browser/time"
 				));
 		JSend resp = JSend.parse(json);
 		String data = resp.getData().string();
@@ -61,6 +62,7 @@ public class AggregationTest {
 		assert ! data.contains("no0");
 	}
 	
+	/*
 	@Test
 	// this will create a new index named datalog.compressed_oct20 
 	public void testBulkNewIndexData() {
@@ -115,19 +117,108 @@ public class AggregationTest {
 		// sanity check if data is written to local ES
 		Map<String, Object> got = esc.get(INDEX, "simple", "compressed_20");
 		System.out.println(got);
+	}*/
+
+	
+	@Test
+	// this will create a new index named datalog.compressed_oct20 
+	public void testBulkNewIndexData2() {
+		initDataTest();
+		FakeBrowser fb = fb();
+		// create breakdown that does aggregation based specified fields
+		String json = fb.getPage(ENDPOINT+"/data", new ArrayMap(
+				"name","test-1",
+				"dataspace", DATASPACE,
+				"breakdown", "evt/campaign/domain/pub/vert/browser/os/time"
+				));
+		JSend resp = JSend.parse(json);
+		String data = resp.getData().string();
+		Printer.out(data);
+		
+		// prepare to bulk aggregated data
+		Dep.setIfAbsent(FlexiGson.class, new FlexiGson());
+		Dep.setIfAbsent(ESConfig.class, new ESConfig());
+		ESConfig esconfig = Dep.get(ESConfig.class);
+		if ( ! Dep.has(ESHttpClient.class)) Dep.setSupplier(ESHttpClient.class, false, ESHttpClient::new);
+		ESHttpClient esc = Dep.get(ESHttpClient.class);
+		BulkRequestBuilder bulk = esc.prepareBulk();
+		
+		// parse aggregated data 
+		JsonElement jelement = new JsonParser().parse(data);
+	    JsonObject  jobject = jelement.getAsJsonObject();
+	    jobject = jobject.getAsJsonObject("by_evt_campaign_domain_pub_vert_browser_os_time");
+	    JsonArray jarray1 = jobject.getAsJsonArray("buckets");
+	    // each j1 is a different evt
+	    int i = 0;
+	    for (JsonElement j1 : jarray1) {
+	    	Pair2<String,JsonArray> evt = parseAggregations(j1, "by_campaign_domain_pub_vert_browser_os_time");
+	    	String evt_name = evt.getFirst();
+	    	JsonArray jarray2 = evt.getSecond();
+	    	for (JsonElement j2 : jarray2) { // each j2 is a campaign
+	    		Pair2<String,JsonArray> campaign = parseAggregations(j2, "by_domain_pub_vert_browser_os_time");
+		    	String campaign_name = campaign.getFirst();
+		    	JsonArray jarray3 = campaign.getSecond();
+		    	for (JsonElement j3 : jarray3) { // each j3 is a domain
+		    		Pair2<String,JsonArray> domain = parseAggregations(j3, "by_pub_vert_browser_os_time");
+			    	String domain_name = domain.getFirst();
+			    	JsonArray jarray4 = domain.getSecond();
+			    	for (JsonElement j4 : jarray4) { // each j4 is a pub
+			    		Pair2<String,JsonArray> pub = parseAggregations(j4, "by_vert_browser_os_time");
+				    	String pub_name = pub.getFirst();
+				    	JsonArray jarray5 = pub.getSecond();
+				    	for (JsonElement j5 : jarray5) { // each j5 is a vert
+				    		Pair2<String,JsonArray> vert = parseAggregations(j5, "by_browser_os_time");
+					    	String vert_name = vert.getFirst();
+					    	JsonArray jarray6 = vert.getSecond();
+					    	for (JsonElement j6 : jarray6) { // each j6 is a browser
+					    		Pair2<String,JsonArray> browser = parseAggregations(j6, "by_os_time");
+						    	String browser_name = browser.getFirst();
+						    	JsonArray jarray7 = browser.getSecond();
+						    	for (JsonElement j7 : jarray7) { // each j7 is an os
+						    		Pair2<String,JsonArray> os = parseAggregations(j7, "by_time");
+							    	String os_name = os.getFirst();
+							    	JsonArray jarray8 = os.getSecond();
+							    	for (JsonElement j8 : jarray8) { // each j8 is a time interval of one day
+							    		String time = j8.getAsJsonObject().get("key_as_string").getAsString();
+							    		double doc_count = j7.getAsJsonObject().get("doc_count").getAsDouble();
+							    		IndexRequestBuilder pi = esc.prepareIndex(INDEX, "compressed_"+i);
+							    		i = i + 1; //increment document id
+							    		pi.setBodyMap(new ArrayMap(
+							    				"evt", evt_name,
+							    				"campaign", campaign_name,
+							    				"domain", domain_name,
+							    				"pub", pub_name,
+							    				"vert", vert_name,
+							    				"browser", browser_name,
+							    				"os", os_name,
+							    				"time", time,
+							    				"count", doc_count));
+										bulk.add(pi);
+							    	}
+						    	}
+					    	}
+				    	}
+			    	}
+		    	}
+	    	}
+	    }
+	    bulk.setRefresh(KRefresh.WAIT_FOR);
+		bulk.setDebug(true);
+		BulkResponse br = bulk.get();
+		assert ! br.hasErrors() : br.getError();
+		
+		// sanity check if data is written to local ES
+		Map<String, Object> got = esc.get(INDEX, "simple", "compressed_20");
+		System.out.println(got);
 	}
-	/* 
-	//troublesome to use
-	private JSend parseJSON(String data, String keyname) {
-		Map jobj = (Map) JSON.parse(data);
-		System.out.println(jobj.keySet());
-		Object _data = jobj.get(keyname); //need to check if keyname is correct
-		JThing thing = new JThing();
-		JSend jsend = new JSend();
-		thing.setJsonObject(_data);
-		JSend resp = jsend.setData(thing);
-		return resp;
-	} */
+	
+	private Pair2<String, JsonArray> parseAggregations(JsonElement j, String term) {
+		JsonObject jobject = j.getAsJsonObject();
+    	String name = jobject.get("key").getAsString();
+    	jobject = jobject.getAsJsonObject(term);
+    	JsonArray jarray = jobject.getAsJsonArray("buckets");
+    	return new Pair2<String, JsonArray>(name, jarray);
+	}
 	
 	private FakeBrowser fb() {
 		FakeBrowser fb = new FakeBrowser();
