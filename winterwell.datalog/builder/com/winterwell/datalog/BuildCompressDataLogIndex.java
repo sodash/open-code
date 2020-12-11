0e69c8fadfc19bc57333dd6307aaa069880a919b
Daniel Winterstein <daniel@good-loop.com>
2020-12-11 15:03:37 +0000

    login-fix

--- winterwell.datalog/builder/com/winterwell/datalog/BuildCompressDataLogIndex.java
+++ winterwell.datalog/builder/com/winterwell/datalog/BuildCompressDataLogIndex.java
@@ -4,12 +4,13 @@ package com.winterwell.datalog;
 import java.util.List;
 
 import com.winterwell.bob.BuildTask;
+import com.winterwell.bob.tasks.WinterwellProjectFinder;
 import com.winterwell.bob.wwjobs.BuildWinterwellProject;
 
 public class BuildCompressDataLogIndex extends BuildWinterwellProject {
 
 	public BuildCompressDataLogIndex() {
-		super(guessProjectDir("winterwell.datalog"), "datalog.compressor");
+		super(new WinterwellProjectFinder().apply("winterwell.datalog"), "datalog.compressor");
 		setMainClass("com.winterwell.datalog.server.CompressDataLogIndexMain");
 	}	
 
