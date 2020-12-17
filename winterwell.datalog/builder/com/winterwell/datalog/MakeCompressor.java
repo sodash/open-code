044aaf36e48e758747e372fc4d8799af000ba9c0
Daniel Winterstein <daniel@good-loop.com>
2020-12-17 17:44:59 +0000

    build-wip

--- winterwell.datalog/builder/com/winterwell/datalog/MakeCompressor.java
+++ winterwell.datalog/builder/com/winterwell/datalog/MakeCompressor.java
@@ -4,14 +4,20 @@ package com.winterwell.datalog;
 import com.winterwell.bob.tasks.WinterwellProjectFinder;
 import com.winterwell.bob.wwjobs.BuildWinterwellProject;
 
-public class BuildCompressor extends BuildWinterwellProject {
+/**
+ * Hm: if this is called BuildX, then Bob will not be able to choose between this and BuildDataLog.
+ * So renamed to MakeX
+ * @author daniel
+ *
+ */
+public class MakeCompressor extends BuildWinterwellProject {
 
 	public static void main(String[] args) throws Exception {
-		BuildCompressor b = new BuildCompressor();
+		MakeCompressor b = new MakeCompressor();
 		b.doTask();
 	}
 	
-	public BuildCompressor() {
+	public MakeCompressor() {
 		super(new WinterwellProjectFinder().apply("winterwell.datalog"), "datalog.compressor");
 		setMainClass("com.winterwell.datalog.server.CompressDataLogIndexMain");
 	}	
