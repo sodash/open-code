0cae3838033c83603490d3e48cb3ec5acd499502
Daniel Winterstein <daniel@good-loop.com>
2021-05-06 13:43:53 +0100

    sync

--- winterwell.utils/src/com/winterwell/datalog/IDataLogAdmin.java
+++ winterwell.utils/src/com/winterwell/datalog/IDataLogAdmin.java
@@ -2,15 +2,6 @@ package com.winterwell.datalog;
 
 public interface IDataLogAdmin {
 
-//	/**
-//	 * @deprecated This is problematic - How to maintain non-standard events across months?
-//	 * So it is (with ESStorage) a no-op
-//	 * 
-//	 * @param dataspace
-//	 * @param eventType
-//	 */
-//	void registerEventType(CharSequence dataspace, String eventType);
-
 	void registerDataspace(CharSequence dataspace);
 	
 }
